ef0bdf433f5f8a528d81d300811d86745d8cafde
delachurch@gmail.com <delachurch@gmail.com@47f95c4b-cb91-dac6-2059-b924b5371797>
2011-12-05 15:31:03 +0000

    Beta 1.1

--- tpalgoritmos/src/proyecto/beans/UniversidadEvent.java
+++ tpalgoritmos/src/proyecto/beans/UniversidadEvent.java
@@ -6,6 +6,24 @@ public class UniversidadEvent {
 	
 	private Universidad universidad;
 	
-	
 
 }
+
+/*package edu.caece.algo2.tp.event;
+
+import edu.caece.algo2.tp.hospital.ambulancia.Ambulancia;
+
+public class CambioAmbulanciaEvent {
+
+	private Ambulancia ambulancia;
+
+	public CambioAmbulanciaEvent(Ambulancia ambulancia) {
+		this.ambulancia = ambulancia;
+	}
+
+	public Ambulancia getAmbulancia() {
+		return ambulancia;
+	}
+	
+}*/
+
