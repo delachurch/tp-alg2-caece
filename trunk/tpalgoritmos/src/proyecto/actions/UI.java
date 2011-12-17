bb0edcd539ed06a87fbf3171424b80855e692455
jeremias.nicolas@gmail.com <jeremias.nicolas@gmail.com@47f95c4b-cb91-dac6-2059-b924b5371797>
2011-12-17 13:17:52 +0000

    se creo el metodo EjecutarUI y se cambio el switch por el if

--- trunk/tpalgoritmos/src/proyecto/actions/UI.java
+++ trunk/tpalgoritmos/src/proyecto/actions/UI.java
@@ -18,6 +18,42 @@ public class UI implements Subscripto{
 		
 	};
 	
+  public void EjecutarUI()
+  {  
+      System.out.println("Ingrese nombre Universidad");
+      ConsoleReader MyReader = new ConsoleReader();
+      ArrayList<String> res = new ArrayList<String>();
+      res = MyReader.getInput();
+      
+      System.out.println(res);
+  		while (res.get(0) != "666") {
+			
+			System.out.println("Ingrese Opcion: ALTA - ACCION ");
+			res = MyReader.getInput();
+			
+      if (res.get(0).equals("ALTA")){
+          System.out.println("Ingrese opcion: CARRERA - MATERIA - ALUMNO");
+				  myUI.MenuAltas(MyReader.getInput());
+      }
+      else if (res.get(0).equals("ACCION")){
+      		System.out.println("Ingrese opcion: CARRERA - MATERIA - ALUMNO");
+				  myUI.MenuAltas(MyReader.getInput());
+      }
+      
+			/*switch (res.get(0)){
+			case "ALTA"   : {
+				System.out.println("Ingrese opcion: CARRERA - MATERIA - ALUMNO");
+				myUI.MenuAltas(MyReader.getInput());
+				break;
+			}
+			case "ACCION" : {
+				System.out.println("Ingrese opcion: INICIOCUATRIMESTRE - FINCUATRIMESTRE - INICIOCURSADA - FINCURSADA");
+				System.out.println("				SolicitudTitulo - ActaDeFinal - RegistrarAlumno - RegistrarAlumno - RegistrarNota - InscribirAlumno");
+				myUI.MenuAcciones(MyReader.getInput());
+				break;
+			}
+			}*/
+  }
 
 	//Menu de Altas con sus respectivas opciones
 	public void MenuAltas(ArrayList<String> res) throws UniversidadException {
