ae47fb82c23bba00f3e2b7b9bbf395aa9612305a
delachurch@gmail.com <delachurch@gmail.com@47f95c4b-cb91-dac6-2059-b924b5371797>
2011-12-17 14:06:55 +0000

    Notificaciones agregadas

--- trunk/tpalgoritmos/src/proyecto/actions/MainAction.java
+++ trunk/tpalgoritmos/src/proyecto/actions/MainAction.java
@@ -15,40 +15,10 @@ public class MainAction {
 	 * @throws UniversidadException 
 	 */
 	public static void main(String[] args) throws UniversidadException {
-		//mover todo el main a la clase UI
-		/*System.out.println("Ingrese nombre Universidad");
-		ConsoleReader MyReader = new ConsoleReader();
-		ArrayList<String> res = new ArrayList<String>();
-		res = MyReader.getInput();
-		
-		System.out.println(res);*/
-		
+				
 		UI myUI = new UI();
 		myUI.EjecutarUI();
 		
-		
-		
-		/*while (res.get(0) != "666") {
-			
-			System.out.println("Ingrese Opcion: ALTA - ACCION ");
-			res = MyReader.getInput();
-			
-			switch (res.get(0)){
-			case "ALTA"   : {
-				System.out.println("Ingrese opcion: CARRERA - MATERIA - ALUMNO");
-				myUI.MenuAltas(MyReader.getInput());
-				break;
-			}
-			case "ACCION" : {
-				System.out.println("Ingrese opcion: INICIOCUATRIMESTRE - FINCUATRIMESTRE - INICIOCURSADA - FINCURSADA");
-				System.out.println("				SolicitudTitulo - ActaDeFinal - RegistrarAlumno - RegistrarAlumno - RegistrarNota - InscribirAlumno");
-				myUI.MenuAcciones(MyReader.getInput());
-				break;
-			}
-			}
-	
-			
-		}*/
 		
 
 	}
--- trunk/tpalgoritmos/src/proyecto/beans/Subscripto.java
+++ trunk/tpalgoritmos/src/proyecto/beans/Subscripto.java
@@ -3,5 +3,7 @@ package proyecto.beans;
 public interface Subscripto {
 	
 		public void onCarreraNueva(Carrera c);
+		public void onAlumnoNuevo(Alumno a);
+		public void onMateriaNueva(Materia m);
 		
 }
