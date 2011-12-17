7b9d077affecf2bbf80b21954d575198c289c275
jeremias.nicolas@gmail.com <jeremias.nicolas@gmail.com@47f95c4b-cb91-dac6-2059-b924b5371797>
2011-12-17 13:05:23 +0000

    Se cambia el switch por If

--- trunk/tpalgoritmos/src/proyecto/beans/Promocion.java
+++ trunk/tpalgoritmos/src/proyecto/beans/Promocion.java
@@ -12,12 +12,18 @@ public class Promocion {
 	}
 
 	public static Promocion getPromocionbyName(String promocion) {
-	 		switch (promocion) {
+       
+       if (promocion.equals("PAR) return new PromocionPar();
+       if (promocion.equals("IMPAR) return new PromocionImpar();
+       if (promocion.equals("NORMAL) return new PromocionNormal();
+       else return null;
+       
+	 		/*switch (promocion) {
 	 		case "PAR" : return new PromocionPar(); 
 	 		case "IMPAR" : return new PromocionImpar(); 
 	 		case "NORMAL" : return new PromocionNormal(); 
 	 		default : return null;
-	 		}
+	 		}*/
 	}		
 	 		
 }
